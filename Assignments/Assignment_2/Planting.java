3758c88ce1b4d6e30d3ed346e489d7f2a8e11665
Felix Singerman <flsingerman@gmail.com>
2018-03-03 13:18:50 -0500

    finished assignment 2

--- Assignments/Assignment_2/Planting.java
+++ Assignments/Assignment_2/Planting.java
@@ -3,15 +3,13 @@
 //
 // The Planting Synchronization Problem
 //
-import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class Planting 
 {
 	public static void main(String args[]) 
-	{
-		int i;
-	 	// Create Student, TA, Professor threads
+	{ 
+		// Create Student, TA, Professor threads
 		TA ta = new TA();
 		Professor prof = new Professor(ta);
 		Student stdnt = new Student(ta);
@@ -34,8 +32,8 @@ class Student extends Thread
 	TA ta;
 
 	public Student(TA taThread)
-        {
-	    ta = taThread;
+	{
+		ta = taThread;
 	}
 
 	public void run()
@@ -43,23 +41,33 @@ class Student extends Thread
 		while(true)
 		{
 
+			System.out.println("Student: Must wait for TA "+ta.getMAX()+" holes ahead");
 
+			try {
+				ta.digStudent.acquire();
+				try {
+					ta.waitShovel.acquireUninterruptibly();
+					// Can dig a hole - lets get the shovel
+					System.out.println("Student: Got the shovel");
 
-			ta.digStudent.acquireUninterruptibly();
-		     System.out.println("Student: Must wait for TA "+ta.getMAX()+" holes ahead");
+					try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
+					ta.incrHoleDug();  // hole filled - increment the number	
+					System.out.println("Student: Hole "+ta.getHoleDug()+" Dug");
 
-		    
-		     // Can dig a hole - lets get the shovel
-		     System.out.println("Student: Got the shovel");
-		     
-	             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
-                     ta.incrHoleDug();  // hole filled - increment the number	
-		     System.out.println("Student: Hole "+ta.getHoleDug()+" Dug");
-		     ta.waitProf.release();
-		     System.out.println("Student: Letting go of the shovel");
-		     
-		    
-		     if(isInterrupted()) break;
+					System.out.println("Student: Letting go of the shovel");
+				}finally{
+					ta.waitShovel.release();
+				}
+
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}finally{
+				ta.waitProf.release();
+			}
+
+			if(isInterrupted()) break;
+
 		}
 		System.out.println("Student is done");
 	}
@@ -78,31 +86,21 @@ class TA extends Thread
 	final Semaphore waitTa;
 	final Semaphore waitProf;
 
-	public int getMAX() { 
-		return(MAX); 
-	}
-	public void incrHoleDug() { 
-		holeDugNum++; 
-	}
-	public int getHoleDug() { 
-		return(holeDugNum); 
-	}
-	public void incrHolePlanted() { 
-		holePlantedNum++; 
-	}
-	public int getHolePlanted() { 
-		return(holePlantedNum); 
-	}
+	public int getMAX() { return(MAX); } 
+	public void incrHoleDug() { holeDugNum++; }
+	public int getHoleDug() { return(holeDugNum); }
+	public void incrHolePlanted() { holePlantedNum++; }
+	public int getHolePlanted() { return(holePlantedNum); }
 
 	public TA()
 	{
-		// Initialise things here
-		waitShovel = new Semaphore(0);
+		// Initialize things here
+		waitShovel = new Semaphore(1,true);
 		digStudent = new Semaphore(MAX,true);
 		waitTa = new Semaphore(0);
 		waitProf = new Semaphore(0);
 	}
-	
+
 	public void run()
 	{
 		while(true)
@@ -110,20 +108,26 @@ class TA extends Thread
 
 			try {
 				waitTa.acquire();
+				try {
+					waitShovel.acquireUninterruptibly();
+					System.out.println("TA: Got the shovel");
+
+					try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
+					holeFilledNum++;  // hole filled - increment the number	
+					System.out.println("TA: The hole "+holeFilledNum+" has been filled");
+					System.out.println("TA: Letting go of the shovel"); //when we can give shovel
+				}finally {
+					waitShovel.release();
+				}
 			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
+			}finally {
+
+				digStudent.release();
+
 			}
-			
-		     System.out.println("TA: Got the shovel");
-		     
-	             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
-                     holeFilledNum++;  // hole filled - increment the number	
-		     System.out.println("TA: The hole "+holeFilledNum+" has been filled");
-		     System.out.println("TA: Letting go of the shovel"); //when we can give shovel
-		    digStudent.release();
-		     
-		     if(isInterrupted()) break;
+
+			if(isInterrupted()) break;
 		}
 		System.out.println("TA is done");
 	}
@@ -134,27 +138,31 @@ class Professor extends Thread
 	TA ta;
 
 	public Professor(TA taThread)
-        {
-	    ta = taThread;
+	{
+		ta = taThread;
 	}
 
 	public void run()
 	{
 		while(ta.getHolePlanted() <= 20)
 		{
-		     try {
+			try {
 				ta.waitProf.acquire();
+				try {sleep((int) (50*Math.random()));} catch (Exception e) { break;} // Time to plant
+				ta.incrHolePlanted();  // the seed is planted - increment the number	
+				System.out.println("Professor: All be advised that I have completed planting hole "+
+						ta.getHolePlanted());
+
 			} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
+			}finally{
+				ta.waitTa.release();
 			}
-	             try {sleep((int) (50*Math.random()));} catch (Exception e) { break;} // Time to plant
-                     ta.incrHolePlanted();  // the seed is planted - increment the number	
-		     System.out.println("Professor: All be advised that I have completed planting hole "+
-				        ta.getHolePlanted());
-		     ta.waitTa.release();
+
 		}
-		
+
 		System.out.println("Professeur: We have worked enough for today");
+
 	}
 }
